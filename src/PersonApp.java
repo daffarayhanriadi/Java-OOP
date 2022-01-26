1bd4e7165f122dda52b73cbc4f6d21d27f41b15f
Daffarr <77198942+xxBumblebee@users.noreply.github.com>
2022-01-26 18:34:45 +0700

    Field

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,3 +1,18 @@
 public class PersonApp {
-    
+    public static void main(String[] args) {
+
+        var person1 = new Person(); // cara 1 buat object
+
+        person1.name = "Daffa";
+        person1.address = "Medan";
+//        person1.country = "Amerika" ERROR karena sudah final
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+        Person person2 = new Person(); // cara 2 buat object
+        Person person3; // cara 3 buat object
+        person3 = new Person();
+    }
 }
