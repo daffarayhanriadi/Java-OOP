1d987d768aa125d989c8902ccac95746998219fa
Daffarr <77198942+xxBumblebee@users.noreply.github.com>
2022-01-26 18:53:39 +0700

    Update PersonApp.java

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,7 +1,7 @@
 public class PersonApp {
     public static void main(String[] args) {
 
-        var person1 = new Person(); // cara 1 buat object
+        var person1 = new Person(); // cara 1 membuat object
 
         person1.name = "Daffa";
         person1.address = "Medan";
@@ -13,8 +13,8 @@ public class PersonApp {
 
         person1.sayHello("Budi");
 
-        Person person2 = new Person(); // cara 2 buat object
-        Person person3; // cara 3 buat object
+        Person person2 = new Person(); // cara 2 membuat object
+        Person person3; // cara 3 membuat object
         person3 = new Person();
         person3.name = "Joko"; // Sehingga bisa di handle dengan ini (kondisi di bawah)
         person3.sayHello("Budi"); // Null karena tipe data yang ada di dalam object belum dideklarasikan
