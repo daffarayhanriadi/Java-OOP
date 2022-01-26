0be4742b630ed37c8ac83ce23c026d144ca0f9f3
Daffarr <77198942+xxBumblebee@users.noreply.github.com>
2022-01-26 18:52:33 +0700

    Update PersonApp.java

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,9 +1,9 @@
 public class PersonApp {
     public static void main(String[] args) {
 
-        var person1 = new Person(); // cara 1
-        Person person2 = new Person(); // cara 1
-        Person person3; // cara 1
+        var person1 = new Person(); // cara 1 membuat object
+        Person person2 = new Person(); // cara 2 membuat object
+        Person person3; // cara 3 membuat object
         person3 = new Person();
 
         System.out.println(person1);
