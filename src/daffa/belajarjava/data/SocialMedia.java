5d758fe23bc45f4d62f1b7d48c9c2f9f42964887
Daffarr <77198942+xxBumblebee@users.noreply.github.com>
2022-02-07 15:02:42 +0700

    Final Method

---
+++ src/daffa/belajarjava/data/SocialMedia.java
@@ -0,0 +1,20 @@
+package daffa.belajarjava.data;
+
+class SocialMedia {
+    String name;
+}
+
+class Facebook extends SocialMedia {
+    final void login(String username, String password) {
+        // isi method
+    }
+}
+
+class FakeFacebook extends Facebook {
+
+    // ERROR
+//    void login(String username, String password) {
+//        // isi method
+//    }
+    
+}
